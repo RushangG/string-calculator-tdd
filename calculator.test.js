eee9fb7d1b8f04ee0a5949c758c76b9239e6fb8c
Gajera Rushang <gajerarushang17@gmail.com>
2025-07-04 23:00:39 +0530

    Add test for exception on negative numbers

--- calculator.test.js
+++ calculator.test.js
@@ -8,11 +8,15 @@ test('single number returns its value', () => {
   expect(add('5')).toBe(5);
 });
 
-
 test('two numbers, comma delimited, returns the sum', () => {
   expect(add('1,2')).toBe(3);
 });
 
 test('supports custom delimiter', () => {
   expect(add('//;\n1;2')).toBe(3);
+});
+
+
+test('throws on negative numbers with message', () => {
+  expect(() => add('1,-2,-3')).toThrow('negative numbers not allowed: -2,-3');
 });
