17d7a73865f73bdc1ac6d3489108108433283d19
Gajera Rushang <gajerarushang17@gmail.com>
2025-07-04 22:43:36 +0530

    Add test for two numbers, comma delimited

--- calculator.test.js
+++ calculator.test.js
@@ -7,3 +7,8 @@ test('empty string returns 0', () => {
 test('single number returns its value', () => {
   expect(add('5')).toBe(5);
 });
+
+
+test('two numbers, comma delimited, returns the sum', () => {
+  expect(add('1,2')).toBe(3);
+});
