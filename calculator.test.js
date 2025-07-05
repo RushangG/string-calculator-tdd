59a00e8a47981afae6524f8387247c1e0eb6ea45
Gajera Rushang <gajerarushang17@gmail.com>
2025-07-05 14:09:08 +0530

    Add advanced and edge-case tests for String Calculator

--- calculator.test.js
+++ calculator.test.js
@@ -1,65 +1,81 @@
 const { add } = require('./calculator');
 
+// Step 1: An empty string returns 0
 test('empty string returns 0', () => {
   expect(add('')).toBe(0);
   expect(add('')).toMatchSnapshot();
 });
 
+// Step 2: A single number returns its value
 test('single number returns its value', () => {
-  expect(add('5')).toBe(5);
-  expect(add('5')).toMatchSnapshot();
+  expect(add('1')).toBe(1);
+  expect(add('1')).toMatchSnapshot();
 });
 
-test('two numbers, comma delimited, returns the sum', () => {
+// Step 3: Two numbers, comma delimited, returns the sum
+test('two numbers returns their sum', () => {
   expect(add('1,2')).toBe(3);
   expect(add('1,2')).toMatchSnapshot();
 });
 
-test('multiple numbers, comma delimited', () => {
-  expect(add('1,2,3,4')).toBe(10);
-  expect(add('1,2,3,4')).toMatchSnapshot();
+// Step 4: An unknown amount of numbers
+test('multiple numbers returns their sum', () => {
+  expect(add('1,2,3')).toBe(6);
+  expect(add('4,5,6,7')).toBe(22);
+  expect(add('1,2,3')).toMatchSnapshot();
+  expect(add('4,5,6,7')).toMatchSnapshot();
 });
 
-test('numbers separated by newlines and commas', () => {
+// Step 5: New lines between numbers (in addition to commas)
+test('numbers separated by commas or newlines', () => {
   expect(add('1\n2,3')).toBe(6);
+  expect(add('4\n5\n6')).toBe(15);
   expect(add('1\n2,3')).toMatchSnapshot();
+  expect(add('4\n5\n6')).toMatchSnapshot();
 });
 
+// Step 6: Support custom single-character delimiter
 test('supports custom single-character delimiter', () => {
   expect(add('//;\n1;2')).toBe(3);
+  expect(add('//-\n4-3')).toBe(7);
   expect(add('//;\n1;2')).toMatchSnapshot();
+  expect(add('//-\n4-3')).toMatchSnapshot();
 });
 
+// Step 7: Negative numbers throw an exception listing all negatives
+test('throws on negative numbers with message', () => {
+  expect(() => add('1,-2,-3')).toThrow('negative numbers not allowed: -2,-3');
+  expect(() => add('//;\n-1;2;-3')).toThrow('negative numbers not allowed: -1,-3');
+});
+
+// Step 8: Numbers bigger than 1000 are ignored
+test('ignores numbers bigger than 1000', () => {
+  expect(add('2,1001')).toBe(2);
+  expect(add('1000,1')).toBe(1001);
+  expect(add('2,1001')).toMatchSnapshot();
+  expect(add('1000,1')).toMatchSnapshot();
+});
+
+// Step 9: Delimiters can be of any length
 test('supports custom multi-character delimiter', () => {
   expect(add('//[***]\n1***2***3')).toBe(6);
+  expect(add('//[abc]\n4abc5abc6')).toBe(15);
   expect(add('//[***]\n1***2***3')).toMatchSnapshot();
+  expect(add('//[abc]\n4abc5abc6')).toMatchSnapshot();
 });
 
-test('supports multiple custom delimiters', () => {
+// Step 10: Multiple delimiters
+test('supports multiple delimiters', () => {
   expect(add('//[*][%]\n1*2%3')).toBe(6);
+  expect(add('//[;][,]\n4;5,6')).toBe(15);
   expect(add('//[*][%]\n1*2%3')).toMatchSnapshot();
+  expect(add('//[;][,]\n4;5,6')).toMatchSnapshot();
 });
 
+// Step 11: Multiple delimiters with length longer than one char
 test('supports multiple multi-character delimiters', () => {
   expect(add('//[***][%%]\n1***2%%3')).toBe(6);
+  expect(add('//[##][@@]\n4##5@@6')).toBe(15);
   expect(add('//[***][%%]\n1***2%%3')).toMatchSnapshot();
-});
-
-test('ignores numbers larger than 1000', () => {
-  expect(add('2,1001')).toBe(2);
-  expect(add('2,1001')).toMatchSnapshot();
-  expect(add('//;\n1000;1;2000')).toBe(1001);
-  expect(add('//;\n1000;1;2000')).toMatchSnapshot();
-});
-
-test('throws on negative numbers with message (single negative)', () => {
-  expect(() => add('1,2,-3')).toThrow('negative numbers not allowed: -3');
-});
-
-test('throws on negative numbers with message (multiple negatives)', () => {
-  expect(() => add('1,-2,-3')).toThrow('negative numbers not allowed: -2,-3');
-});
-
-test('throws on negative numbers with custom delimiter', () => {
-  expect(() => add('//;\n-1;2')).toThrow('negative numbers not allowed: -1');
+  expect(add('//[##][@@]\n4##5@@6')).toMatchSnapshot();
 });
